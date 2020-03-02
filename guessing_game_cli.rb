eb6c88394b238eeb9c7461c775780a2e1d2dc6c4
Michael Wixson <wixsonmc@gmail.com>
2020-03-02 17:08:54 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,5 +1,6 @@
 def run_guessing_game
   our_number = rand(1..6)
+  puts "Guess a number between 1 and 6:"
   user_number = gets.chomp
   if user_number == our_number
     puts "You guessed the correct number!"
