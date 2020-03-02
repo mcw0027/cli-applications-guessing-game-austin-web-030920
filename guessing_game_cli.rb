ce91f16506e7cea57d4efb26de0ebfa908fe2e03
Michael Wixson <wixsonmc@gmail.com>
2020-03-02 17:09:11 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -2,7 +2,7 @@ def run_guessing_game
   our_number = rand(1..6)
   puts "Guess a number between 1 and 6:"
   user_number = gets.chomp
-  if user_number == our_number
+  if user_number == our_number.to_s
     puts "You guessed the correct number!"
   elsif user_number == "exit"
     puts "Goodbye!"
